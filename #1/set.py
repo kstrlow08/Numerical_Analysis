07bfd9b472f50bd6ec1a543265d6c97fb08c4091
kstrlow08 <115918558+kstrlow08@users.noreply.github.com>
2024-11-12 22:30:10 +0900

    Add files via upload

---
+++ #1/set.py
@@ -0,0 +1,7 @@
+styles = {
+    'euler': {'color': 'blue', 'linestyle': '-', 'marker': 'o'},
+    'improved_euler': {'color': 'green', 'linestyle': '--', 'marker': 'x'},
+    'rk4': {'color': 'red', 'linestyle': '-.', 'marker': 's'},
+    'taylor': {'color': 'purple', 'linestyle': ':', 'marker': 'd'},
+    'ls': {'color': 'orange', 'linestyle': '-', 'marker': ''}
+}
